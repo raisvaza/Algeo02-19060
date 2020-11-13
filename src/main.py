35ffc85e8f55d45226c2126feb16a6a71b20dd41
Siti Iedrania <68504848+iedrania@users.noreply.github.com>
2020-11-13 23:36:53 +0700

    tambahan minor, editing minor

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,12 @@ def about():
 @app.route('/upload/')
 def upload():
    return render_template('upload.html')
+
+@app.route('/result/', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
 
 if __name__ == '__main__':
    app.run(debug = True)
