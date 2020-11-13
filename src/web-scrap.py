5dbfe19a984146d8a6c4ecf2d6b70b5031e27b12
Rais Vaza <raisvaza@gmail.com>
2020-11-13 21:21:33 +0700

    Stemming and Removing stopwords

--- src/web-scrap.py
+++ src/web-scrap.py
@@ -20,7 +20,7 @@ nama_file = nama_file.replace("?","") + ".txt"
 # Ekstraksi konten artikel dan pembersihan konten dari iklan dan foto
 konten = soup.find(True, "read__content")
 
-konten_bersih = konten.find_all(["p","h2","h3"])
+konten_bersih = konten.find_all(["p","h2","h3","ol","ul"])
 for paragraf in konten_bersih:
     if paragraf.find(class_=["inner-link-baca-juga","photo"]):
         paragraf.decompose()
