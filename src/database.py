4df87eb611e0a6ead4a3e7f16c1b11fd37ecaf31
Siti Iedrania <68504848+iedrania@users.noreply.github.com>
2020-11-15 15:45:29 +0700

    database done

--- src/database.py
+++ src/database.py
@@ -8,31 +8,13 @@ jumlahKataDokumen = []
 
 direktoriDokumen = "../test"
 
-def isiNamaDokumen(dokumen):
-# Memasukkan nama dokumen ke array namaDokumen
-    namaDokumen.append(dokumen)
-
-def isiJudulDokumen(dokumen):
-# Memasukkan judul dokumen ke array judulDokumen
-    judulDokumen.append(dokumen)
-
 def isiKontenDokumen(dokumen):
 # Memasukkan judul dokumen ke array kontenDokumen
     konten = ""
     for p in dokumen:
         if p != judul:
-             konten = konten + p
+            konten = konten + p
     kontenDokumen.append(konten)
-
-def isiParagrafPertamaDokumen(indeksDokumen):
-# Memasukkan judul dokumen ke array paragrafPertamaDokumen
-    paragrafPertama = ""
-    for char in kontenDokumen[indeksDokumen]:
-        if char != "\n" :
-            paragrafPertama = paragrafPertama + char
-        else:
-            break
-    paragrafPertamaDokumen.append(paragrafPertama)
 
 def isiJumlahKataDokumen(judulDokumen,konten):
 # Memasukkan judul dokumen ke array jumlahKataDokumen
@@ -41,26 +23,25 @@ def isiJumlahKataDokumen(judulDokumen,konten):
 
 for filename in os.listdir(direktoriDokumen):
     if filename.endswith(".txt"):
-
-        isiNamaDokumen(filename)
+        namaDokumen.append(filename)
 
         f = open(direktoriDokumen + "/" + filename, "r")
-        
-        judul = f.readline()
-        isiJudulDokumen(judul)
 
+        judul = f.readline()
+        judulDokumen.append(judul)
+
+        isiArtikel = f.readline()
         isiArtikel = f.readlines()
         isiKontenDokumen(isiArtikel)
 
-        indeksDokumen = 0
-        isiParagrafPertamaDokumen(indeksDokumen)
-        
-        isiJumlahKataDokumen(jumlahKataDokumen[indeksDokumen],kontenDokumen[indeksDokumen])
-        indeksDokumen += 1
+        f.close()
+        f = open(direktoriDokumen + "/" + filename, "r")
+        judul = f.readline()
+        pertama = f.readline()
+        pertama = f.readline()
 
-database = []
-database.append(namaDokumen)
-database.append(judulDokumen)
-database.append(kontenDokumen)
-database.append(paragrafPertamaDokumen)
-database.append(jumlahKataDokumen)
+        paragrafPertamaDokumen.append(pertama)
+        
+        isiJumlahKataDokumen(judul,isiArtikel)
+
+database = [namaDokumen, judulDokumen, kontenDokumen, paragrafPertamaDokumen, jumlahKataDokumen]
